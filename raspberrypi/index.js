b35ec9e2e497e43a3283a8c1b97199495cea9c8f
WORKGROUP\ta-yagi <ta-yagi@kentem.co.jp>
2025-12-22 10:19:38 +0900

    const化

--- raspberrypi/index.js
+++ raspberrypi/index.js
@@ -1,44 +1,44 @@
 import axios from "axios";
 import { readGPIO } from "./gpio/infraredSensor.js";
 import { recordVideo } from "./camera/recorder.js";
+import { CONSTS } from "./constant/const.js";
 
 let prevValue = 0;
 let isRecording = false;
-
 setInterval(async () => {
-  const value = readGPIO();
+    const value = readGPIO();
 
-  if (prevValue === 0 && value === 1 && !isRecording) {
-    isRecording = true;
+    if (prevValue === 0 && value === 1 && !isRecording) {
+        isRecording = true;
 
-    console.log("👀 人を検知しました。サーバーへ送信します。");
+        console.log("👀 人を検知しました。サーバーへ送信します。");
 
-    try {
-      await axios.post(
-        "http://192.168.32.164:3000/infrared/post",
-        {
-          value,
-          detectedAt: new Date().toISOString(),
-        },
-        {
-          timeout: 5000,
+        try {
+            await axios.post(
+                `http://${CONSTS.LOCAL_SERVER_IP}:3000/infrared/post`,
+                {
+                    value,
+                    detectedAt: new Date().toISOString(),
+                },
+                {
+                    timeout: 5000,
+                }
+            );
+
+            console.log("✅ 送信成功。録画を開始します。");
+
+            const ffmpeg = recordVideo();
+
+            ffmpeg.on("close", () => {
+                console.log("🛑 録画終了");
+                isRecording = false;
+            });
+
+        } catch (err) {
+            console.error("❌ 送信失敗:", err.message);
+            isRecording = false;
         }
-      );
-
-      console.log("✅ 送信成功。録画を開始します。");
-
-      const ffmpeg = recordVideo();
-
-      ffmpeg.on("close", () => {
-        console.log("🛑 録画終了");
-        isRecording = false;
-      });
-
-    } catch (err) {
-      console.error("❌ 送信失敗:", err.message);
-      isRecording = false;
     }
-  }
 
-  prevValue = value;
+    prevValue = value;
 }, 100);
